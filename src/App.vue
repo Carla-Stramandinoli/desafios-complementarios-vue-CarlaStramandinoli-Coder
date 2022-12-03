42147faf1daf879006cc28481e90f35781c1e021
Carla Stramandinoli <carla.stram@gmail.com>
2022-12-02 21:39:07 -0300

    inicializo el repo y realizo el desafio complementario, agrego los archivos.

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="app" class="m-4 px-2">
+    <componentes-tabla :alumnos="alumnosPrimero" :estilo="estilo1"/>
+    <componentes-tabla :alumnos="alumnosSegundo" :estilo="estilo2"/>
+    <componentes-tabla :alumnos="alumnosTercero" :estilo="estilo3"/>
+  </div>
+</template>
+
+<script>
+import ComponentesTabla from './components/ComponentesTabla.vue'
+
+export default {
+  name: "app",
+  components: {
+    ComponentesTabla
+  },
+  data() {
+    return {
+      alumnosPrimero: [
+        {
+          nombre: "Olga Mayor",
+          nota: 9
+        },
+        {
+          nombre: "Alejo Acuña",
+          nota: 6
+        },
+        {
+          nombre: "Lorena Manso",
+          nota: 10
+        },
+      ],
+      alumnosSegundo: [
+        {
+          nombre: "Leonardo Serra",
+          nota: 6
+        },
+        {
+          nombre: "Axel Jimeno",
+          nota: 4
+        },
+        {
+          nombre: "Laura Trillo",
+          nota: 8
+        },
+      ],
+      alumnosTercero: [
+        {
+          nombre: "Mariano Orellana",
+          nota: 5
+        },
+        {
+          nombre: "Christian Arevalo",
+          nota: 7
+        },
+        {
+          nombre: "Emilia Fernndez",
+          nota: 7
+        },
+      ],
+      estilo1: ["table", "table-success"],
+      estilo2: ["table", "table-dark"],
+      estilo3: ["table", "table-striped"],
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/ComponentesTabla.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <table :class="estilo">
+      <thead :class="estilo">
+        <tr>
+          <th>Nombre</th>
+          <th>Nota</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(alumnos, index) of alumnos" :key="index">
+          <td>{{ alumnos.nombre }}</td>
+          <td>{{ alumnos.nota }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ComponentesTabla',
+  props: {
+    alumnos: Array,
+    estilo: Array
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
